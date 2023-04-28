5deb9fdaf586cb4eb68c5c818066d6bd1ced8dc2
Anízio Campos Pereira <aniziocp@poli.ufrj.br>
2023-04-28 00:30:52 -0300

    Third commit
    In this commit I added a new Jupyter notebook file which we will keep working in our project.

--- toronto-real-state.py
+++ toronto-real-state.py
@@ -1,3 +1,7 @@
-from selenium import webdriver
 
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(r"/Users/aniziocp/.cache/selenium/chromedriver/")
+driver = webdriver.Chrome(service=service)
+
